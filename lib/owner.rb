4811b7b027b38b3dd9cadf846fe8dbf388fe33d0
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-24 22:21:05 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -60,7 +60,7 @@ class Owner
   end
 
   def list_pets
-    return "I have #{@self.dogs.count} dog(s), and #{self.cats.count} cat(s)."
+    return "I have #{@self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
   end
 
 end
