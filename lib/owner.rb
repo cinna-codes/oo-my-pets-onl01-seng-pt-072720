eb523f4cd6100b6ecb6f9ca9b466bec776c96c32
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-24 22:11:10 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 #require 'pry'
 class Owner
-  attr_accessor #:all_pets
-  attr_reader :species, :name
+  attr_accessor :all_pets
+  attr_reader :owners_cats, :owners_dogs, :species, :name
 
   @@all_owners = []
 
@@ -9,6 +9,8 @@ class Owner
     @name = name
     @species = "human"
     #@all_pets = []
+    @owners_cats = []
+    @owners_dogs = []
     @@all_owners << self
   end
 
@@ -42,6 +44,11 @@ class Owner
     # Cat.all.find { |c| c.name == name }.owner = self
     # self.all_pets << Cat.all.find { |c| c.name == name }
 
+    # When an owner buys a new pet, the buy_cat/buy_dog methods take in an argument of a name. You must take that name and do the following:
+    # Make a new instance of the appropriate pet, initializing it with that name and the owner who is purchasing it.
+
+    
+    
   end
 
 end
