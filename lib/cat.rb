b30707dc80e2ef9251097d8ed774b7abbe524623
Cinna Goldman <somethingfellipickeditup@gmail.com>
2020-08-24 21:43:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -3,10 +3,13 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all_cats = []
+
   def initialize(name, owner)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all_cats << self
   end
 
 
